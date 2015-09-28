2e4f3c629c5fd21a4e190836e9468d10a426ad67
kelxvel <o.p.oliynyk@gmail.com>
2015-09-28 20:24:10 +0300

    fix slug

--- product/models.py
+++ product/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.utils import timezone
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length = 128, unique = True)
-    slug = models.SlugField(blank = True, unique = True)
+    slug = models.SlugField(blank = True)
     description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name) 
+        super(Category, self).save(*args, **kwargs)
 
 class Product(models.Model):
     name = models.CharField(max_length = 128, unique = True)
-    slug = models.SlugField(blank = True, unique = True)
+    slug = models.SlugField(blank = True)
     description = models.TextField()
     price = models.FloatField()
     created_at = models.DateTimeField(default = timezone.now)
@@ -17,3 +22,7 @@ class Product(models.Model):
     def modify(self):
         self.modified_at = timezone.now()
         self.save()
+   
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
