cb112755487c0174c57be5c5f8834fdc2ec4c265
kelxvel <o.p.oliynyk@gmail.com>
2015-09-28 18:21:33 +0300

    added models: category and product

---
+++ product/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+                ('slug', models.SlugField(unique=True)),
+                ('description', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+                ('slug', models.SlugField(unique=True)),
+                ('description', models.TextField()),
+                ('price', models.FloatField()),
+                ('created_at', models.DateTimeField(default=django.utils.timezone.now)),
+                ('modified_at', models.DateTimeField(null=True, blank=True)),
+            ],
+        ),
+    ]
--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique = True)
+    slug = models.SlugField(unique = True)
+    description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length = 128, unique = True)
+    slug = models.SlugField(unique = True)
+    description = models.TextField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(default = timezone.now)
+    modified_at = models.DateTimeField(blank = True, null = True)
+
+    def modify(self):
+        self.modified_at = timezone.now()
+        self.save()
