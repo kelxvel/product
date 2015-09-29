82f4ae0f21414c2ecd9999267117c199c429f074
kelxvel <o.p.oliynyk@gmail.com>
2015-09-29 21:27:11 +0300

    added product details

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^products$', views.category_list),
-    url(r'^products/(?P<slug>.*)/$', views.category_detail),
+    url(r'^products/$', views.category_list),
+    url(r'^products/(?P<category_slug>.+)/$', views.category_detail, name='category_detail'),
+    url(r'^products/(?P<category_slug>.+)/(?P<product_slug>.+)/$', views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -5,7 +5,11 @@ def category_list(request):
     categories = Category.objects.all()
     return render(request, 'category_list.html', {'categories': categories})    
 
-def category_detail(request, slug):
-    category = Category.objects.get(slug = slug)
+def category_detail(request, category_slug):
+    category = Category.objects.get(slug = category_slug)
     products = Product.objects.filter(category = category)
     return render(request, 'category_detail.html', {'products': products})
+
+def product_detail(request, category_slug, product_slug):
+    product = Product.objects.get(slug = product_slug)
+    return render(request, 'product_detail.html', {'product': product})
