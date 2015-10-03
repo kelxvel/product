672f9e196c3e9f259a279948071926e6d71c90b1
kelxvel <kelxvel@gmail.com>
2015-10-03 22:45:59 +0300

    changed urls order

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.root_page),
     url(r'^products/$', views.category_list),
-    url(r'^products/(?P<category_slug>.+)/$', views.category_detail, name='category_detail'),
     url(r'^products/(?P<category_slug>.+)/(?P<product_slug>.+)/$', views.product_detail, name='product_detail'),
+    url(r'^products/(?P<category_slug>.+)/$', views.category_detail, name='category_detail'),
     url(r'^new_products/$', views.new_products),
 ]
